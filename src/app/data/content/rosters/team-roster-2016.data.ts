a279f9dc3a32c66ea1094fe0d0c5c99eab8e8be2
Michael Ezekiel <mike@ezekiels.org>
2016-11-12 09:54:55 -0500

    2016 team roster

--- src/app/data/content/rosters/team-roster-2016.data.ts
+++ src/app/data/content/rosters/team-roster-2016.data.ts
@@ -3,6 +3,28 @@ import { TeamRoster } from '../../../team-roster/TeamRoster';
 export const TEAM_ROSTER_2016: TeamRoster[] = [
     {
         year: 2016,
+        name: 'Calcite',
+        division: 'Club',
+        ageGroup: '15U',
+        headCoach: 'Docmigh Thoummavong',
+        assistantCoaches: ['Lauren Geary'],
+        players: ['Boyer, Madelyn','Brandner, Ashley','Flynn, Avery','Flynn, Faith','Guerra, Natalia','Hitchcock, Nicole','Hurtado, Kindred','Locke, Renee','Pineiro, Angelina','Scudder, Paige','Westhaver, Arianna'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Citrine',
+        division: 'Club',
+        ageGroup: '16U',
+        headCoach: 'Cheryl Kwinn',
+        assistantCoaches: ['Lauren Bennet'],
+        players: ['Callahan, Shayla','Chang, Shina','Kukkal, Rhea','Kustka, Erin','Montion, Erin','Padellaro, Alexandra','Peters, Brooklyn','Som, Audrianna','Tamura, Anna','Tracy, Abby','Xie, Kristin'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
         name: 'Crimson',
         division: 'Open',
         ageGroup: '18U',
@@ -36,6 +58,105 @@ export const TEAM_ROSTER_2016: TeamRoster[] = [
     },
     {
         year: 2016,
+        name: 'Indigo',
+        division: 'Club',
+        ageGroup: '16U',
+        headCoach: 'Mikaela MacKinnon',
+        assistantCoaches: ['Kaltelyn Marr'],
+        players: ['Bessette, Hannah','Boyle, Maxine','Calnan, Sarah','Gonzalez, Emely','Kotler, Elana','Maher, Allison','McPherson, Ali','Shin, Suhyun','Svendsen, Michaela','Tomei, Alison','West, Cassandra'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Jade',
+        division: 'Club',
+        ageGroup: '18U',
+        headCoach: 'Brian Healey',
+        assistantCoaches: ['Rachel Arnason','Guy Tucker','Alicia Bursey'],
+        players: ['Badger, Jessica','Cataldo, Marissa','Cooper, Katalinn','Cooper, Kellie','Crovo, Victoria','Desrochers, Gabrielle','Doyle, Sabrina','Groele, Jamie','Huynh, Dalena','MacInnes, Kaitlyn','Petherbridge, Nicole','Shattuck, Lynn'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Jasper',
+        division: 'Open',
+        ageGroup: '17U',
+        headCoach: 'Michele Naughton',
+        assistantCoaches: ['Marla Marcum','Alexandria Toth'],
+        players: ['Antczak, Katherine','Coen, Jen','Doan, Andra','Dykeman, Ashlie','Farnum, Kathryn','Flynn, Lauren','Geary, Emma','Gotts, Brittany','Kimball, Ali','McNally, Megan','Zargaj, Julia'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Maize',
+        division: 'Club',
+        ageGroup: '18U',
+        headCoach: 'Joseph Seid',
+        assistantCoaches: ['Kaltelyn Marr'],
+        players: ['Cassidy, Jacqueline','Chisholm, Delaney','Cullen, Delanie','DAddario, Lindsey','Favreau, Adrianna','Griffin, Cailee','LoConte, Katrina','Martin, Fiona','Mucci, Laura','Noyes, Christina','Trabucco, Luciana','Zhang, Qiqi'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Midnight',
+        division: 'Club',
+        ageGroup: '16U',
+        headCoach: 'Brianna Karpinski',
+        assistantCoaches: ['Kim Crawford','Elima Ahzi'],
+        players: ['Burliss, Ryder','Caputo, Grace','Caswell, Emory','Costa, Sarah','Doolin, Emily','Farris, Isabella','Frampton, Katherine','Gonzalez, Yarimar','Jens, Meagan','Naatz, Brooke','Orchanian, Rita'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Onyx',
+        division: 'Club',
+        ageGroup: '18U',
+        headCoach: 'Anny Din',
+        assistantCoaches: ['Pedro Figueroa'],
+        players: ['Albanese, Rebecca','Amodei, Amelia','Barnes, Virginia','Bricknell, Jordan','Butler, Nora','Cheverie, Erin','McDonnell, Hailey','McGrath, Jennifer','Riccardi, Lauren','Zhang, Vanessa'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Peridot',
+        division: 'Club',
+        ageGroup: '14U',
+        headCoach: 'Sue Rafferty',
+        assistantCoaches: ['Sophie Meyers','Kristen Sgrosso'],
+        players: ['Byrne, Caitlin','Cioto, Gianna','Coutu, Stella','Determan, Allison','Fox, Caitlyn','Goldthwaite, Neena','Kidder, Chloe','King, Autumn','Murphy, Lauren','Schena, Isabelle','Sgrosso, Allison'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Ruby',
+        division: 'Club',
+        ageGroup: '16U',
+        headCoach: 'Jim Ray',
+        assistantCoaches: ['Ali Remagio'],
+        players: ['Bresnahan, Morgan','He, Stephanie','Karim, Julia','Kirkland, Alexis','OConnor, Meghan','Shattuck, Sarah','Steed, MaryRose','Wang, Sophia','Williams, Sarah'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Sage',
+        division: 'Club',
+        ageGroup: '16U',
+        headCoach: 'Emily Martel',
+        assistantCoaches: ['Danielle McDonald','Julie OCoin'],
+        players: ['Butler, Catherine','Gaspar, Olivia','Kurr, Leah','Lamothe, Brooke','Li, Joy','Plodzik, Jamie','Redmond, Fiona','Schramm, Katelyn','Stevens, Taylor','Tang, Sunny','Xu, Isabella'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
         name: 'Sapphire',
         division: 'Club',
         ageGroup: '15U',
@@ -44,5 +165,49 @@ export const TEAM_ROSTER_2016: TeamRoster[] = [
         players: ['Butler, Emily','Cheng, Cathy','DeGeorge, Samantha','Ervin, Kaitlin','Ezekiel, Elizabeth','Farnum, Laura','Harrison, Nicole','Hu, Grace','Shea, Kerry','Som, Pichanny','Thawnghmung, Dai'],
         teamPhoto: 'assets/img/teams/2016/Sapphire 15.jpg',
         teamPhotoThumbNail: 'assets/img/teams/2016/Sapphire 15 NERVA V Gold-thumbnail.jpg'
+    },
+    {
+        year: 2016,
+        name: 'Starlite',
+        division: 'Club',
+        ageGroup: '14U',
+        headCoach: 'Emily Cox',
+        assistantCoaches: ['Erin Cox','Rachel McIntosh'],
+        players: ['Byers, Jenna','Chang, Anica','Geary, Brynn','Green, Niamh','Hamilton, Johanna','McDonald, Kayleigh','Morin, Katherine','Savio, Julia','Warner, Riley','Willoughby, Lillian'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Steel Blue',
+        division: 'Open',
+        ageGroup: '16U',
+        headCoach: 'Alison Martin',
+        assistantCoaches: ['Lauren Yung'],
+        players: ['Blake, Willa','Fahey, Christa','Farnum, Sarah','Fox, Morgan','Hill, Bree','Houck, Cordelia','Johnston, Julia','Knight, Natalie','Tang, Alicia','Williams, Flannery'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Tanzanite',
+        division: 'Club',
+        ageGroup: '12U',
+        headCoach: 'Rosella Latam',
+        assistantCoaches: ['Clara Reich'],
+        players: ['Blandford, Samantha','Carmody, Taylor','Coutu, Eva','Featherston, Renee','Husak, Nina','Latam, Vanessa','Mcdonald, Lindsay','Morin, Elizabeth','Schiaparelli, Emma','Trant, Tierney','Traveis, Cameron'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2016,
+        name: 'Viridian',
+        division: 'Open',
+        ageGroup: '17U',
+        headCoach: 'Dan OConnor',
+        assistantCoaches: ['Sarah Shainwald', 'Sophia Basuri'],
+        players: ['Ada, Ruth','Bluhm, Sarah','Caruso, Victoria','Davies, Rachel','Dawley, Megan','Green, Alicia','Keiver, Abigail','McCloskey, Nicole','OConnor, Allison','Stewart, Natasha'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
     }
 ];
