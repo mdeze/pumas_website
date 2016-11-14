3ba079e1216539e4ca975cb17ca10b38ea49f335
Michael Ezekiel <mike@ezekiels.org>
2016-11-13 22:45:11 -0500

    updated tournament schedule to be data driven

---
+++ src/app/schedule-tournament/TournamentItem.ts
@@ -0,0 +1,12 @@
+export class TournamentItem {
+    id: number;
+    season: string;
+    name: string;
+    url: string;
+    date: Date;
+    dateOnDisplay: string;
+    location: string;
+    info: string;
+    displayIcon: string;
+    displayIconStyle: string;
+}
--- src/app/schedule-tournament/schedule-tournament.component.ts
+++ src/app/schedule-tournament/schedule-tournament.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TournamentItem } from './TournamentItem';
+import { TournamentScheduleService } from '../data/services/schedule-tournament.service';
+
 @Component({
   selector: 'app-schedule-tournament',
   templateUrl: './schedule-tournament.component.html',
-  styleUrls: ['./schedule-tournament.component.css']
+  styleUrls: ['./schedule-tournament.component.css'],
+  providers: [TournamentScheduleService]
 })
 export class ScheduleTournamentComponent implements OnInit {
+  tournamentItems: TournamentItem[]
 
-  constructor() { }
+  constructor(private tournamentService: TournamentScheduleService) { }
 
   ngOnInit() {
+    this.tournamentItems = this.tournamentService.getTournamentSchedule();
   }
 
 }
