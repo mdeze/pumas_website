a7b3b154aa7f42dc483d7fc71dd6027cc3522c14
Michael Ezekiel <mike@ezekiels.org>
2016-11-23 12:19:24 -0500

    enabled caret toggle in accordion #16

--- src/app/club-faq/club-faq.component.ts
+++ src/app/club-faq/club-faq.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-club-faq',
   templateUrl: './club-faq.component.html',
   styleUrls: ['./club-faq.component.css']
 })
-export class ClubFaqComponent implements OnInit {
+export class ClubFaqComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(){
+    // collapse panels after load - required for gmaps render
+    setTimeout(
+      $('.panel-collapse').each(function(){
+          if ($(this).hasClass('in')) {
+              $(this).collapse('toggle');
+          }
+      })
+    , 500);
+
+    // enable toggle caret on panel expand
+    $('.accordion-toggle').on('click', function() {
+      let $icon = $(this).find('i');
+      if ($icon.hasClass('fa-caret-right')) {
+        $icon.removeClass('fa-caret-right').addClass('fa-caret-down');
+      } else {
+        $icon.addClass('fa-caret-right').removeClass('fa-caret-down');
+      }
+    });
   }
 
 }
