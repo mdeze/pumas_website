f38b0fb3b99db325d9c8e37bf45249589ec2595e
Michael Ezekiel <mike@ezekiels.org>
2016-12-03 23:25:21 -0500

    added stub 2017 team roster data

---
+++ src/app/data/content/rosters/team-roster-2017.data.ts
@@ -0,0 +1,125 @@
+import { TeamRoster } from '../../../team-roster/TeamRoster';
+
+export const TEAM_ROSTER_2017: TeamRoster[] = [
+    {
+        year: 2017,
+        name: 'Crimson',
+        division: 'Open',
+        ageGroup: '18U',
+        headCoach: 'Stephen Boyle',
+        assistantCoaches: [],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Ebony',
+        division: 'Club',
+        ageGroup: '15U',
+        headCoach: 'Steve McRoy',
+        assistantCoaches: ['Bonnie Zeigler'],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Emerald',
+        division: 'Open',
+        ageGroup: '16U',
+        headCoach: 'Bruce Shainwald',
+        assistantCoaches: ['Bridget Shainwald'],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Indigo',
+        division: 'Club',
+        ageGroup: '15U',
+        headCoach: 'Mikaela MacKinnon',
+        assistantCoaches: [],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Jade',
+        division: 'Club',
+        ageGroup: '18U',
+        headCoach: 'Brian Healey',
+        assistantCoaches: [],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Maize',
+        division: 'Club',
+        ageGroup: '18U',
+        headCoach: 'Joseph Seid',
+        assistantCoaches: [],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Midnight',
+        division: 'Club',
+        ageGroup: '16U',
+        headCoach: 'Brianna Karpinski',
+        assistantCoaches: [],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Ruby',
+        division: 'Club',
+        ageGroup: '18U',
+        headCoach: 'Jim Ray',
+        assistantCoaches: [],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Sapphire',
+        division: 'Club',
+        ageGroup: '16U',
+        headCoach: 'Mike Ezekiel',
+        assistantCoaches: [],
+        players: ['Ezekiel, Elizabeth'],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Tanzanite',
+        division: 'Club',
+        ageGroup: '12U',
+        headCoach: 'Rosella Latam',
+        assistantCoaches: [],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    },
+    {
+        year: 2017,
+        name: 'Viridian',
+        division: 'Club',
+        ageGroup: '18U',
+        headCoach: "Dan O'Connor",
+        assistantCoaches: ['Sarah Shainwald'],
+        players: [],
+        teamPhoto: '',
+        teamPhotoThumbNail: ''
+    }
+];
--- src/app/data/services/team-roster.service.ts
+++ src/app/data/services/team-roster.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { TeamRoster } from '../../team-roster/TeamRoster';
 
+import { TEAM_ROSTER_2017 } from '../content/rosters/team-roster-2017.data';
 import { TEAM_ROSTER_2016 } from '../content/rosters/team-roster-2016.data';
 import { TEAM_ROSTER_2015 } from '../content/rosters/team-roster-2015.data';
 import { TEAM_ROSTER_2014 } from '../content/rosters/team-roster-2014.data';
@@ -11,6 +12,8 @@ export class TeamRosterService {
 
     getRosterByYear(year: number): TeamRoster[] {
         switch (year) {
+            case 2017:
+                return TEAM_ROSTER_2017;
             case 2016:
                 return TEAM_ROSTER_2016;
             case 2015:
