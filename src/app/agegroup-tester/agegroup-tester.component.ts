eb831306bc5bfabae75e5d59f2b4bbe254fb6849
Michael Ezekiel <mike@ezekiels.org>
2017-10-16 09:34:38 -0400

    bug fix & refactor age tester logic so that it's easier to maintain and is future proof

--- src/app/agegroup-tester/agegroup-tester.component.ts
+++ src/app/agegroup-tester/agegroup-tester.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var moment: any;
+
 @Component({
   selector: 'app-agegroup-tester',
   templateUrl: './agegroup-tester.component.html',
   styleUrls: ['./agegroup-tester.component.css']
 })
 export class AgegroupTesterComponent implements OnInit {
-  playerMonth: number = 1;
-  playerDay: number = 1;
-  playerYear: number = 1994;
-  calculatedPlayerAgeGroup: string = '';
+  playerMonth = 1;
+  playerDay = 1;
+  playerYear = 1994;
+  calculatedPlayerAgeGroup = '';
 
   days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  years = [1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006];
   months = [
     { number: 1, name: 'January' },
     { number: 2, name: 'February' },
@@ -27,10 +28,31 @@ export class AgegroupTesterComponent implements OnInit {
     { number: 11, name: 'November' },
     { number: 12, name: 'December' }
   ];
+  years = [];
+  currentYear = moment().year();
+
+  ages = [18, 17, 16, 15, 14, 13, 12, 11, 10, 9];
+  ageGroups = [];
 
   constructor() { }
 
   ngOnInit() {
+    // load available birth years for supported age groups
+    for (const age of this.ages) {
+      this.years.push(this.currentYear - age);
+    }
+    // load age group start dates
+    this.loadAgeGroups();
+  }
+
+  // load age group start day, are you x age on 09/01
+  // in 2017, a 16U player' birthdate in on or after 09/01/2001
+  loadAgeGroups() {
+    for (const age of this.ages) {
+      const ageGroupStartDate = (this.currentYear - age) + '-09-01';
+      const group = age + 'U';
+      this.ageGroups.push({ageGroup: group, startDate: ageGroupStartDate});
+    }
   }
 
   setMonth(month: number) {
@@ -45,76 +67,42 @@ export class AgegroupTesterComponent implements OnInit {
     this.playerYear = year;
   }
 
+  // is your birthday on or after 9/1/CCYY
   calculateAgeGroup() {
-    let birthdate: string = this.playerMonth + '/' + this.playerDay + '/' + this.playerYear;
-    let ageGroup = '';
+    const birthdate: string = this.playerMonth + '/' + this.playerDay + '/' + this.playerYear;
+    let ageGroup = 'UNK';
+    let ageGroupDisplay = birthdate + ' would play in unavailable age group. Contact Club Director.';
 
-    /* Comparison date for this season */
-    let compareMonth = 8;
-    let compareDay = 31;
-
-    /* If this years season is complete, use next year for checking. For simplicity,
-      assume anything later than compareMonth means the season is over */
-
-    let now = new Date();
-    let compareYear = now.getFullYear();
-    let thisMonth = now.getMonth();
-
-    if (thisMonth >= compareMonth) {
-      compareYear++;
-    }
-
-    let	years = compareYear - this.playerYear;
-
-    if (compareMonth <= this.playerMonth) {
-      if (this.playerMonth === compareMonth) {
-        if (this.playerDay > compareDay) {
-          years--;
-        } else {
-          years--;
-        }
+    for (const age of this.ageGroups) {
+      if (moment(birthdate).isSameOrAfter(age.startDate)) {
+        ageGroup = age.ageGroup;
       }
     }
 
-    let unknownAge = false;
-
-    switch (years) {
-      case 18:
-        ageGroup = '18U';
-        break;
-      case 17:
-        ageGroup = '17/18U';
-        break;
-      case 16:
-        ageGroup = '16U';
-        break;
-      case 15:
-        ageGroup = '15U';
-        break;
-      case 14:
+    // normalize age groups
+    // 14U -> 13 & 14, 12U -> 9-12
+    switch (ageGroup) {
+      case '13U':
         ageGroup = '14U';
         break;
-      case 13:
-        ageGroup = '14U';
+      case '11U':
+        ageGroup = '12U';
         break;
-      case 12:
+      case '10U':
+        ageGroup = '12U';
+        break;
+      case '9U':
         ageGroup = '12U';
         break;
       default:
-        /* Set <= 12 to 12's Age Group */
-        if (years <= 12) {
-          ageGroup = '12U';
-        } else {
-          ageGroup = 'unknown';
-          unknownAge = true;
-        }
+        ageGroup = ageGroup;
     }
 
-    if (unknownAge) {
-      this.calculatedPlayerAgeGroup = birthdate + ' would play in unavailable age group. Contact Club Director.';
-    } else {
-      this.calculatedPlayerAgeGroup = birthdate + ' would play in ' + ageGroup + ' age group';
+    if (ageGroup !== 'UNK') {
+      ageGroupDisplay = birthdate + ' would play in ' + ageGroup + ' age group';
     }
+
+    this.calculatedPlayerAgeGroup = ageGroupDisplay;
   }
 
 }
