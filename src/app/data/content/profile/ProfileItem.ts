9dd6d1f0d1b4568867dcdf363598dcc43a58717c
Michael Ezekiel <mike@ezekiels.org>
2017-10-26 09:25:29 -0400

    coach profile display updates

--- src/app/data/content/profile/ProfileItem.ts
+++ src/app/data/content/profile/ProfileItem.ts
@@ -1,10 +1,26 @@
 export class ProfileItem {
+    public picture: string;
+    public thumbnail: string;
+    public title: string;
+    public division: string;
+    public team: string;
+    public years: number;
+
+    public summary: string;
 
     constructor(
         public id: number,
         public firstName: string,
         public lastName: string,
-        public thumbnail: string,
-        public title: string
-    ) {}
+        public isHeadCoach: boolean
+    ) {
+        // defaults
+        this.thumbnail = '/assets/img/coach.png';
+        this.picture = '/assets/img/coach.png';
+        this.title = (isHeadCoach) ? 'Head Coach' : 'Asst Coach';
+        this.division = 'TBD';
+        this.team = 'TBD';
+        this.years = 0;
+        this.summary = 'Profile summary not avilable';
+    }
 }
